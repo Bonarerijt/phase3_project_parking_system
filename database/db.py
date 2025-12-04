b26ec9818a3213d461f825ba46307b1306b84f28
Bonarerijt <prudyjj4@gmail.com>
2025-12-04 21:19:50 +0300

    Organizing database file

--- database/db.py
+++ database/db.py
@@ -1,12 +1,18 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, Float, Boolean
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
 
 Base = declarative_base()
-engine = create_engine("sqlite:///database/parking.db")
+engine = create_engine("sqlite:///parking.db", echo = False)
 Session = sessionmaker(bind=engine)
 
-def init_db():
-    Base.metadata.create_all(engine)
-def get_session():
-    return Session()
+# My Models
+class Slot(Base):
+    __tablename__ = "slots"
+    id = Column(Integer(), primary_key=True)
+    name = Column(String, unique=True)
+    group = Column(String)
+    is_free = Column(Boolean, default=True)
+    plate = Column(String, nullable=True)
+    entry_time = Column(DateTime, nullable=True)
