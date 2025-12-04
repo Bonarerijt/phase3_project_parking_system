ed2c33a3b5e20816e42a63d80b35b91dc00d480b
Bonarerijt <prudyjj4@gmail.com>
2025-12-04 21:28:48 +0300

    added parenthesis

--- database/db.py
+++ database/db.py
@@ -11,18 +11,18 @@ Session = sessionmaker(bind=engine)
 class Slot(Base):
     __tablename__ = "slots"
     id = Column(Integer(), primary_key=True)
-    name = Column(String, unique=True)
-    group = Column(String)
-    is_free = Column(Boolean, default=True)
-    plate = Column(String, nullable=True)
-    entry_time = Column(DateTime, nullable=True)
+    name = Column(String(), unique=True)
+    group = Column(String())
+    is_free = Column(Boolean(), default=True)
+    plate = Column(String(), nullable=True)
+    entry_time = Column(DateTime(), nullable=True)
 
     class ParkingHistory(Base):
         __tablename__= "parking_histories"
         id = Column(Integer, primary_key = True)
-        plate = Column(String)
-        slot_name = Column(String)
-        entry_time = Column(DateTime)
-        exit_time = Column(DateTime)
-        amount_paid = Column(Float)
+        plate = Column(String())
+        slot_name = Column(String())
+        entry_time = Column(DateTime())
+        exit_time = Column(DateTime())
+        amount_paid = Column(Float())
         
